cdc601d3a940c1a3a5561c55d8f5379ab6d02bb4
Bizzysammy <145840373+Bizzysammy@users.noreply.github.com>
2024-07-02 07:40:22 +0300

    Add files via upload

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,7 +3,16 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  cloud_firestore
+  file_selector_windows
+  firebase_auth
   firebase_core
+  firebase_storage
+  geolocator_windows
+  permission_handler_windows
+  printing
+  url_launcher_windows
+  zego_express_engine
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
